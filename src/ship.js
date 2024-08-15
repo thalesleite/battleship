44c7f7b89ed3eca9e9f138ec2491513b01a16024
thalesleite <thalesaleite@gmail.com>
2024-08-15 14:42:22 +0100

    implement functions for lentgh of ship, hits and to check if it is sunk

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,26 @@
-export default class Ship {
+class Ship {
   constructor(length) {
     this.length = length
+    this.hits = 0
+  }
+
+  getLength() {
+    return this.length
+  }
+
+  getHits() {
+    return this.hits
+  }
+
+  hit() {
+    this.hits += 1
+  }
+
+  isSunk() {
+    if (this.hits >= this.length) return true
+
+    return false
   }
 }
+
+export default Ship
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,13 +1,32 @@
 import Ship from "./ship"
-jest.mock("./ship") // This mocks the entire Ship module
 
 describe("Ship class tests", () => {
   it("Check if the class is called", () => {
-    // Replace the constructor with a mock
-    const mockShipConstructor = jest.fn()
-    Ship.mockImplementation(mockShipConstructor)
-
     const ship = new Ship(4)
-    expect(mockShipConstructor).toHaveBeenCalledTimes(1)
+    expect(ship).toBeInstanceOf(Ship)
+  })
+
+  it("Check if the class returns the correct length", () => {
+    const ship = new Ship(4)
+    expect(ship.getLength()).toBe(4)
+  })
+
+  it("Check if the class returns the correct numnber of hits", () => {
+    const ship = new Ship(4)
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    expect(ship.getHits()).toBe(3)
+
+    ship.hit()
+    ship.hit()
+    expect(ship.getHits()).toBe(5)
+  })
+
+  it("Check if the class returns whether it is sunk", () => {
+    const ship = new Ship(2)
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBe(true)
   })
 })
